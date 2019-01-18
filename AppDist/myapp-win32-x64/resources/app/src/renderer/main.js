4e72dbac01b7a576e04db664ecbefd278981f6c9
dean <814731008@qq.com>
2019-01-18 12:41:28 +0800

    配置element-ui，修复打包exe会出现的问题

--- AppDist/myapp-win32-x64/resources/app/src/renderer/main.js
+++ AppDist/myapp-win32-x64/resources/app/src/renderer/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 
 import App from './App'
-
+import './element-ui'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
---
+++ src/renderer/element-ui.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import {
+  Button,
+  Select,
+  Input
+} from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Input)
+Vue.use(Select)
